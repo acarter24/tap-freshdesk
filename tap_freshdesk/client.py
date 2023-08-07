82f4cb1aa2471f4cd22ba66f4e4cf34fb3a5bd5d
acarter24 <126154605+acarter24@users.noreply.github.com>
2023-08-07 08:18:51 +0100

    Update client.py
    Schema for embeds in config

--- tap_freshdesk/client.py
+++ tap_freshdesk/client.py
@@ -1,223 +1,87 @@
-"""REST client handling, including freshdeskStream base class."""
+"""freshdesk tap class."""
 
 from __future__ import annotations
 
+from singer_sdk import Tap
+from singer_sdk import typing as th  # JSON schema typing helpers
+
+# TODO: Import your custom stream types here:
+from tap_freshdesk import streams
+
+
 from pathlib import Path
-from typing import Any, Callable, Iterable, TYPE_CHECKING, Generator
+config = Path(__file__).parent.parent / '.secrets' / 'config.json'
 
-import requests
-from singer_sdk.authenticators import BasicAuthenticator
-from singer_sdk.helpers.jsonpath import extract_jsonpath
-from singer_sdk.streams import RESTStream
-from singer_sdk.pagination import BasePageNumberPaginator, SinglePagePaginator
-
-if TYPE_CHECKING:
-    from requests import Response
-
-_Auth = Callable[[requests.PreparedRequest], requests.PreparedRequest]
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+_ticket_ids = set()
 
 
-class FreshdeskStream(RESTStream):
-    """freshdesk stream class."""
-    name: str
-    records_jsonpath = "$.[*]"  # Or override `parse_response`.
-    primary_keys = ["id"]
+class Tapfreshdesk(Tap):
+    """freshdesk tap class."""
 
-    @property
-    def backoff_max_tries(self) -> int:
-        return 10
+    name = "tap-freshdesk"
 
-    @property
-    def path(self) -> str:
-        """
-        'groups' -> '/groups'
-        """
-        return f'/{self.name}'
+    # TODO: Update this section with the actual config values you expect:
+    config_jsonschema = th.PropertiesList(
+        th.Property(
+            "api_key",
+            th.StringType,
+            required=True,
+            secret=True,  # Flag config as protected.
+            description="The token to authenticate against the API service",
+        ),
+        th.Property(
+            "start_date",
+            th.DateTimeType,
+            description="The earliest record date to sync",
+        ),
+        th.Property(
+            "domain",
+            th.StringType,
+            description="The url for the API service",
+        ),
+        th.Property(
+            "embeds",
+            th.ObjectType(
+                th.Property(
+                    'tickets_detail', th.ArrayType(
+                        th.StringType(allowed_values=['requester', 'company', 'stats', 'sla_policy'])
+                        )
+                ),
+            ),
+            description='Dictionary of embeds for each stream, see more at https://developers.freshdesk.com/api/'
+        ),
+        ## these two below to enable stream mapping
+        th.Property(
+            "stream_maps",
+            th.ObjectType(),
+        ),
+        th.Property(
+            "stream_map_config",
+            th.ObjectType(),
+        ),
+    ).to_dict()
 
-    @property
-    def schema_filepath(self) -> Path | None:
-        return SCHEMAS_DIR / f'{self.name}.json'
-
-    # OR use a dynamic url_base:
-    @property
-    def url_base(self) -> str:
-        """Return the API URL root, configurable via tap settings."""
-        domain = self.config["domain"]
-        return f"https://{domain}.freshdesk.com/api/v2"
-
-
-    @property
-    def authenticator(self) -> BasicAuthenticator:
-        """Return a new authenticator object.
-
-        Returns:
-            An authenticator instance.
-        """
-        return BasicAuthenticator.create_for_stream(
-            self,
-            username=self.config.get("api_key", ""),
-            password="",
-        )
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed.
-
-        Returns:
-            A dictionary of HTTP headers.
-        """
-        headers = {}
-        if "user_agent" in self.config:
-            headers["User-Agent"] = self.config.get("user_agent")
-        # If not using an authenticator, you may also provide inline auth headers:
-        # headers["Private-Token"] = self.config.get("auth_token")
-        return headers
-
-    def get_next_page_token(
-        self,
-        response: requests.Response,
-        previous_token: Any | None,
-    ) -> Any | None:
-        """Return a token for identifying next page or None if no more pages.
-
-        Args:
-            response: The HTTP ``requests.Response`` object.
-            previous_token: The previous page token value.
-
-        Returns:
-            The next pagination token.
-        """
-        # TODO: If pagination is required, return a token which can be used to get the
-        #       next page. If this is the final page, return "None" to end the
-        #       pagination loop.
-        if self.next_page_token_jsonpath:
-            all_matches = extract_jsonpath(
-                self.next_page_token_jsonpath, response.json()
-            )
-            first_match = next(iter(all_matches), None)
-            next_page_token = first_match
-        else:
-            next_page_token = response.headers.get("X-Next-Page", None)
-
-        return next_page_token
-
-    def get_url_params(
-        self,
-        context: dict | None,
-        next_page_token: Any | None,
-    ) -> dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization.
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary of URL query parameters.
-        """
-        params: dict = {}
-        embeds = self.config.get('embeds')
-        if embeds:
-            embed_fields = embeds.get(self.name, [])
-            if embed_fields:
-                params['include'] = ','.join(embed_fields)
-        return params
-
-    def prepare_request_payload(
-        self,
-        context: dict | None,
-        next_page_token: Any | None,
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
-
-    def parse_response(self, response: requests.Response) -> Iterable[dict]:
-        """Parse the response and return an iterator of result records.
-
-        Args:
-            response: The HTTP ``requests.Response`` object.
-
-        Yields:
-            Each record from the source.
-        """
-        # TODO: Parse response body and return a set of records.
-        yield from extract_jsonpath(self.records_jsonpath, input=response.json())
-
-    def post_process(self, row: dict, context: dict | None = None) -> dict | None:
-        """As needed, append or transform raw data to match expected structure.
-
-        Args:
-            row: An individual record from the stream.
-            context: The stream context.
-
-        Returns:
-            The updated record dictionary, or ``None`` to skip the record.
-        """
-        # TODO: Delete this method if not needed.
-        return row
-    
-    def get_new_paginator(self) -> SinglePagePaginator:
-        return SinglePagePaginator()
-    
-    def backoff_wait_generator(self) -> Generator[float, None, None]:
-        return self.backoff_runtime(value=self._wait_for)
-    
-    @staticmethod
-    def _wait_for(exception) -> int:
-        """
-        When 429 thrown, header contains the time to wait before
-        the next call is allowed, rather than use exponential backoff"""
-        return int(exception.response.headers['Retry-After'])
-    
-    def backoff_jitter(self, value: float) -> float:
-        return value
-
-class FreshdeskPaginator(BasePageNumberPaginator):
-
-    def has_more(self, response: Response) -> bool:
-        """
-        There is no 'has more' indicator for this stream.
-        If there are no results on this page, then this is 'last' page, 
-        (even though technically the page before was the last, there was no way to tell).
-        """
-        return len(response.json())
     
 
-class PagedFreshdeskStream(FreshdeskStream):
-
-    def get_url_params(
-        self,
-        context: dict | None,
-        next_page_token: Any | None,
-    ) -> dict[str, Any]:
-        """Return a dictionary of values to be used in URL parameterization.
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
+    def discover_streams(self) -> list[streams.FreshdeskStream]:
+        """Return a list of discovered streams.
 
         Returns:
-            A dictionary of URL query parameters.
+            A list of discovered streams.
         """
-        params = super().get_url_params(context, next_page_token)
-        params['per_page'] = 100
-        if next_page_token:
-            params["page"] = next_page_token
-        if 'updated_since' not in context:
-            params['updated_since'] = self.get_starting_timestamp(context)
-        return params
+        return [
+            streams.AgentsStream(self),
+            streams.CompaniesStream(self),
+            streams.TicketFieldsStream(self),
+            streams.GroupsStream(self),
+            streams.ContactsStream(self),
+            streams.TicketsAbridgedStream(tap=self, ticket_ids=_ticket_ids),
+            streams.TicketsDetailStream(tap=self, ticket_ids=_ticket_ids),
+            streams.ConversationsStream(self),
+        ]
 
-    def get_new_paginator(self) -> BasePageNumberPaginator:
-        return FreshdeskPaginator(start_value=1)
+
+
+
+if __name__ == "__main__":
+    Tapfreshdesk(config=config).cli()
